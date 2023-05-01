50f81d60e1a58a124f881e8424772ff4946c8f6f
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2023-05-01 20:48:45 -0300

    feat: add Toolbar

--- app/src/main/java/com/viniciusrodriguesaro/carry/MainActivity.kt
+++ app/src/main/java/com/viniciusrodriguesaro/carry/MainActivity.kt
@@ -2,10 +2,28 @@ package com.viniciusrodriguesaro.carry
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.viniciusrodriguesaro.carry.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupToolbar()
+    }
+
+    private fun setupToolbar() {
+        setSupportActionBar(binding.toolbar)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(binding.navHostMainContent.id) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
     }
 }
--- app/src/main/java/com/viniciusrodriguesaro/carry/ui/SigninFragment.kt
+++ app/src/main/java/com/viniciusrodriguesaro/carry/ui/SigninFragment.kt
@@ -5,9 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.viniciusrodriguesaro.carry.R
+import com.viniciusrodriguesaro.carry.databinding.FragmentSigninBinding
 
 class SigninFragment : Fragment() {
+    private var _binding: FragmentSigninBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -16,7 +21,13 @@ class SigninFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signin, container, false)
+        _binding = FragmentSigninBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.signupButton.setOnClickListener { _ -> findNavController().navigate(R.id.action_signinFragment_to_signupFragment) }
     }
 }
