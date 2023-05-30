6aa2a51a11723307e04e2908975a59449e99ebb3
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2023-05-30 02:18:55 -0300

    feat: add currency mask to price input in NewShoppingItemFragment

--- app/src/main/java/com/viniciusrodriguesaro/carry/shoppingitem/ui/NewShoppingItemFragment.kt
+++ app/src/main/java/com/viniciusrodriguesaro/carry/shoppingitem/ui/NewShoppingItemFragment.kt
@@ -1,6 +1,8 @@
 package com.viniciusrodriguesaro.carry.shoppingitem.ui
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -14,6 +16,8 @@ import com.viniciusrodriguesaro.carry.databinding.FragmentNewShoppingItemBinding
 import com.viniciusrodriguesaro.carry.shoppingitem.data.FirestoreShoppingItemRepository
 import com.viniciusrodriguesaro.carry.shoppingitem.dto.CreateShoppingItemInput
 import com.viniciusrodriguesaro.carry.shoppingitem.dto.UnitOfMeasurement
+import java.text.NumberFormat
+import java.text.ParseException
 
 class NewShoppingItemFragment : Fragment() {
     private var _binding: FragmentNewShoppingItemBinding? = null
@@ -22,6 +26,8 @@ class NewShoppingItemFragment : Fragment() {
     private val viewModel: ShoppingItemViewModel by activityViewModels {
         ShoppingItemViewModel.Factory(FirestoreShoppingItemRepository)
     }
+
+    private var previousText = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,6 +57,67 @@ class NewShoppingItemFragment : Fragment() {
         binding.nameEditText.addTextChangedListener {
             binding.submitButton.isEnabled = !it.isNullOrEmpty()
         }
+
+        addPriceFormatterMaskToPriceInput()
+    }
+
+    private fun addPriceFormatterMaskToPriceInput() {
+        val formatter = NumberFormat.getCurrencyInstance()
+
+        val priceTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                binding.priceEditText.removeTextChangedListener(this)
+
+                var isDeletingText: Boolean
+
+                /* Check if user is pressing backspace */
+                if (s.toString().length < previousText.length) {
+                    isDeletingText = true
+                    binding.priceEditText.setText(previousText);
+                    binding.priceEditText.setSelection(previousText.length);
+                } else {
+                    isDeletingText = false
+                }
+
+                var priceInt = s.toString().toIntOrNull()
+
+                /* Parse string to int */
+                try {
+                    if (isDeletingText) {
+                        val parsedString = formatter.parse(previousText)
+                        priceInt =
+                            (parsedString.toDouble() * 100).toInt().toString().dropLast(1).toInt()
+                    } else {
+                        val parsedString = formatter.parse(s.toString())
+                        val last = s?.lastOrNull().toString().toIntOrNull()
+                        priceInt =
+                            ((parsedString.toDouble() * 100).toInt()
+                                .toString() + last).toIntOrNull()
+                    }
+                } catch (e: ParseException) {
+                } catch (e: java.lang.NumberFormatException) {
+                    priceInt = 0
+                }
+
+                /* Format int to currency */
+                if (priceInt != null) {
+                    val formatted = formatter.format(priceInt / 100.0)
+
+                    previousText = formatted
+
+                    binding.priceEditText.setText(formatted)
+                    binding.priceEditText.setSelection(formatted.length)
+                }
+
+                binding.priceEditText.addTextChangedListener(this)
+            }
+        }
+
+        binding.priceEditText.addTextChangedListener(priceTextWatcher)
     }
 
     private fun handleOnCreatePress() {
